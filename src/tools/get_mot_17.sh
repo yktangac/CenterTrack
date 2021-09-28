a3d24b721a064a560e91cea3a19d9d4c59e58b80
YiuKuen <69826761+yktangac@users.noreply.github.com>
2021-09-28 11:26:43 +0800

    Update get_mot_17.sh

--- src/tools/get_mot_17.sh
+++ src/tools/get_mot_17.sh
@@ -5,8 +5,7 @@ unzip MOT17.zip
 mv MOT17 mot17
 rm MOT17.zip
 cd mot17
-cd test
 mkdir annotations
-cd ../../../src/tools/
+cd ../../src/tools/
 python convert_mot_to_coco.py
 python convert_mot_det_to_results.py
